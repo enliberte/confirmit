0f6c302042f55b6bf690beb796039a0cc152d83a
enliberte <zugife21@gmail.com>
2019-06-14 22:32:50 +0700

    'static'

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 
+app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/dist'));
-app.use(express.static(__dirname + '/src/img'));
 
 
 app.get('*', (req, res) => {
